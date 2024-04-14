421cf62945dda7facab1c837dd04a1f5e8bfa582
botprzemek <info@botprzemek.pl>
2024-04-14 15:49:10 +0200

    build(app): added repo and port checking script, basic setup for services and nginx setup with http/s ports

--- basic-setup.sh
+++ basic-setup.sh
@@ -12,13 +12,6 @@ sudo usermod -aG docker "$username"
 sudo apt update
 sudo apt upgrade -y
 
-hasDocker=$(sudo docker -v)
-
-if [ ! "${hasDocker}" ];
-then
-  curl -sSL https://get.docker.com/ | CHANNEL=stable bash
-fi
-
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
 sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
@@ -26,5 +19,13 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubc
 sudo apt install -y gh firewalld
 
 sudo systemctl enable --now firewall-cmd
+
+hasDocker=$(sudo docker -v)
+
+if [ ! "${hasDocker}" ];
+then
+  curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+fi
+
 sudo systemctl enable --now docker
 sudo systemctl enable --now containerd
