efcf7f33d016181b09e22c7e3fa0a1012dd8b2ad
botprzemek <info@botprzemek.pl>
2024-04-14 17:01:17 +0200

    build(app): added repo and port checking script, basic setup for services and nginx setup with http/s ports

--- roach-setup.sh
+++ roach-setup.sh
@@ -15,30 +15,32 @@ joined="${subnet}-${sub}:26357"
 for ((sub=clusters-1; sub>=0; sub--));
 do
   joined="${joined},${subnet}-${sub}:26357"
-  echo "${joined}"
 done
 
 for ((roach=1; roach<=clusters; roach++));
 do
   name="${subnet}-${roach}"
+  echo "${name}:2625${roach}"
   docker volume create "${name}"
   docker run                                     \
     --detach                                     \
-    --name "${name}"                               \
-    --hostname="${subnet}"                         \
-    --net="${subnet}"                                 \
-    --publish "2625${roach}:2625${roach}"      \
+    --name "${name}"                             \
+    --hostname="${subnet}"                       \
+    --net="${subnet}"                            \
+    --publish "2625${roach}:2625${roach}"        \
     --volume "${name}:/cockroach/cockroach-data" \
-    --restart always                           \
-    "$image" \
-    start \
-      --advertise-addr="${name}:26357" \
-      --http-addr="${name}:808${roach}" \
-      --listen-addr="${name}:26357" \
-      --sql-addr="${name}:2625${roach}" \
-      --insecure \
-      --join="${joined}"
-
+    --restart always                             \
+    "$image"                                     \
+    start                                        \
+    --advertise-addr="${name}:26357"             \
+    --http-addr="${name}:808${roach}"            \
+    --listen-addr="${name}:26357"                \
+    --sql-addr="${name}:2625${roach}"            \
+    --insecure                                   \
+    --join="${joined}"
 done
 
-docker exec -it "${subnet}-1" ./cockroach --host=${subnet}-1:26357 init --insecure
+docker exec -it "${subnet}-1" ./cockroach \
+  --host="${subnet}-1:26357"              \
+  init                                    \
+  --insecure
