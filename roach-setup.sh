cd856765c6401a295bebe7e7e0a8917d66a48d80
botprzemek <info@botprzemek.pl>
2024-04-14 17:16:28 +0200

    build(app): added repo and port checking script, basic setup for services and nginx setup with http/s ports

--- roach-setup.sh
+++ roach-setup.sh
@@ -10,11 +10,11 @@ docker pull "${image}"
 
 docker network create -d bridge "${subnet}"
 
-joined="${subnet}-${sub}:26357"
+joined="${subnet}-${sub}:60009"
 
 for ((sub=clusters-1; sub>=0; sub--));
 do
-  joined="${joined},${subnet}-${sub}:26357"
+  joined="${joined},${subnet}-${sub}:60009"
 done
 
 for ((roach=1; roach<=clusters; roach++));
@@ -25,29 +25,29 @@ do
   docker stop "${name}"
   docker rm "${name}"
 
-  docker volume rm "${name}"
-
   docker volume create "${name}"
-  docker run                                     \
-    --detach                                     \
-    --name "${name}"                             \
-    --hostname="${subnet}-${roach}"              \
-    --net="${subnet}"                            \
-    --publish "2625${roach}:2625${roach}"        \
-    --volume "${name}:/cockroach/cockroach-data" \
-    --restart always                             \
-    "$image"                                     \
-    start                                        \
-    --advertise-addr="${name}:26357"             \
-    --http-addr="${name}:808${roach}"            \
-    --listen-addr="${name}:26357"                \
-    --sql-addr="${name}:2625${roach}"            \
-    --insecure                                   \
+
+  docker run                                      \
+    --detach                                      \
+    --name "${name}"                              \
+    --hostname="${subnet}-${roach}"               \
+    --net="${subnet}"                             \
+    --publish "127.0.0.1:808${roach}:808${roach}" \
+    --publish "0.0.0.0:2625${roach}:2625${roach}" \
+    --volume "${name}:/cockroach/cockroach-data"  \
+    --restart always                              \
+    "$image"                                      \
+    start                                         \
+    --advertise-addr="${name}:60009"              \
+    --http-addr="${name}:808${roach}"             \
+    --listen-addr="${name}:60009"                 \
+    --sql-addr="${name}:2625${roach}"             \
+    --insecure                                    \
     --join="${joined}"
 done
 
 docker exec                               \
   -it "${subnet}-1" ./cockroach           \
-  --host="${subnet}-1:26357"              \
+  --host="${subnet}-1:60009"              \
   init                                    \
   --insecure
