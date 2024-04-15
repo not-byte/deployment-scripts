62117d4230b2a7319b5c8c9612149b9fc5c114a9
Przemysław Szymański <przemyslaw.szymanski@flytronic.pl>
2024-04-15 12:39:09 +0200

    roach

--- roach-setup.sh
+++ roach-setup.sh
@@ -5,13 +5,13 @@
 image="cockroachdb/cockroach"
 subnet="notroach"
 controller="${subnet}-1"
-port=60009
+ports=(26357 8081 26257 60008 60009)
 
 if [ "$1" ];
 then
-    clusters=$1
+  clusters=$1
 else
-    clusters=3
+  clusters=3
 fi
 
 docker pull "${image}" &>/dev/null
@@ -21,15 +21,40 @@ docker network create -d bridge "${subnet}" &>/dev/null
 docker container prune --force &>/dev/null
 docker image prune --force &>/dev/null
 
-joined="${controller}:${port}"
+joined="${controller}:${ports[2]}"
 
-for ((roach=1; roach<=clusters; roach++));
+for ((roach=2; roach<=clusters; roach++));
 do
-  if [ "${roach}" -ne 1 ];
   then
-    joined="${joined},${subnet}-${roach}:${port}"
+    joined="${joined},${subnet}-${roach}:${ports[2]}"
   fi
+done
 
+docker stop "${controller}" &>/dev/null
+docker rm "${controller}" &>/dev/null
+
+docker volume create "${controller}" &>/dev/null
+
+docker run                                           \
+  --detach                                           \
+  --name "${controller}"                             \
+  --hostname "${controller}"                         \
+  --net "${subnet}"                                  \
+  --publish "${ports[3]}:${ports[1]}"                \
+  --publish "${ports[4]}:${ports[2]}"                \
+  --volume "${controller}:/cockroach/cockroach-data" \
+  --restart always                                   \
+  "$image"                                           \
+  start                                              \
+  --advertise-addr="${name}:${ports[0]}"             \
+  --http-addr="${controller}:${ports[1]}"            \
+  --listen-addr="${name}:${ports[0]}"                \
+  --sql-addr="${controller}:${ports[2]}"             \
+  --join="${joined}"                                 \
+  --insecure &>/dev/null
+
+for ((roach=2; roach<=clusters; roach++));
+do
   name="${subnet}-${roach}"
 
   docker stop "${name}" &>/dev/null
@@ -37,27 +62,24 @@ do
 
   docker volume create "${name}" &>/dev/null
 
-  docker run                                     \
-    --detach                                     \
-    --name "${name}"                             \
-    --hostname "${subnet}-${roach}"              \
-    --net "${subnet}"                            \
-    --publish "6000${roach}:6000${roach}"        \
-    --publish "6000${roach+3}:6000${roach+3}"    \
-    --volume "${name}:/cockroach/cockroach-data" \
-    --restart always                             \
-    "$image"                                     \
-    start                                        \
-    --advertise-addr="${name}:${port}"           \
-    --http-addr="${name}:6000${roach+3}"         \
-    --listen-addr="${name}:${port}"              \
-    --sql-addr="${name}:6000${roach}"            \
-    --join="${joined}"                           \
+  docker run                                           \
+    --detach                                           \
+    --name "${name}"                                   \
+    --hostname "${subnet}-${roach}"                    \
+    --net "${subnet}"                                  \
+    --volume "${name}:/cockroach/cockroach-data"       \
+    --restart always                                   \
+    "$image"                                           \
+    start                                              \
+    --advertise-addr="${name}:${ports[0]}"             \
+    --listen-addr="${name}:${ports[0]}"                \
+    --sql-addr="${controller}:${ports[2] + roach - 1}" \
+    --join="${joined}"                                 \
     --insecure &>/dev/null
 done
 
-docker exec                       \
-  -it "${controller}" ./cockroach \
-  --host="${controller}:${port}"  \
-  init                            \
+docker exec                          \
+  -it "${controller}" ./cockroach    \
+  --host="${controller}:${ports[0]}" \
+  init                               \
   --insecure
