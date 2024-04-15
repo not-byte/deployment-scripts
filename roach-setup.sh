b21de3767b632e9d8634d9d08bbef6f34a8858b8
Przemysław Szymański <przemyslaw.szymanski@flytronic.pl>
2024-04-15 14:47:48 +0200

    roach

--- roach-setup.sh
+++ roach-setup.sh
@@ -21,7 +21,7 @@ else
   ports[4]=60009
 fi
 
-source utils/port-check.sh "${ports[4]}"
+source utils/port-check.sh $((ports[4]))
 
 docker pull "${image}" &>/dev/null
 
@@ -30,11 +30,11 @@ docker network create -d bridge "${subnet}" &>/dev/null
 docker container prune --force &>/dev/null
 docker image prune --force &>/dev/null
 
-joined="${controller}:${ports[0]}"
+joined="$((controller+":"+ports[0]))"
 
 for ((roach=2; roach<=clusters; roach++));
 do
-    joined="${joined},${subnet}-${roach}:${ports[0]}"
+    joined="$((joined+","+subnet+"-"+roach+":"+ports[0]))"
 done
 
 docker stop "${controller}" &>/dev/null
@@ -42,24 +42,21 @@ docker rm "${controller}" &>/dev/null
 
 docker volume create "${controller}" &>/dev/null
 
-echo "${ports[3]}:${ports[1]}"
-echo "${ports[4]}:${ports[2]}"
-
 docker run                                           \
   --detach                                           \
   --name "${controller}"                             \
   --hostname "${controller}"                         \
   --net "${subnet}"                                  \
-  --publish "${ports[3]}:${ports[1]}"                \
-  --publish "${ports[4]}:${ports[2]}"                \
+  --publish $((ports[3]+":"+ports[1]))               \
+  --publish $((ports[4]+":"+ports[2]))               \
   --volume "${controller}:/cockroach/cockroach-data" \
   --restart always                                   \
   "$image"                                           \
   start                                              \
-  --advertise-addr="${controller}:${ports[0]}"       \
-  --http-addr="${controller}:${ports[1]}"            \
-  --listen-addr="${controller}:${ports[0]}"          \
-  --sql-addr="${controller}:${ports[2]}"             \
+  --advertise-addr="$((controller+":"+ports[0]))"    \
+  --http-addr="$((controller+":"+ports[1]))"         \
+  --listen-addr="$((controller+":"+ports[0]))"       \
+  --sql-addr="$((controller+":"+ports[2]))"          \
   --join="${joined}"                                 \
   --insecure
 
@@ -82,15 +79,15 @@ do
     --restart always                             \
     "$image"                                     \
     start                                        \
-    --advertise-addr="${name}:${ports[0]}"       \
-    --listen-addr="${name}:${ports[0]}"          \
+    --advertise-addr="$((name+":"+ports[0]))"    \
+    --listen-addr="$((name+":"+ports[0]))"       \
     --sql-addr="${name}:${sql}"                  \
     --join="${joined}"                           \
     --insecure
 done
 
-docker exec                          \
-  -it "${controller}" ./cockroach    \
-  --host="${controller}:${ports[0]}" \
-  init                               \
+docker exec                             \
+  -it "${controller}" ./cockroach       \
+  --host="$((controller+":"+ports[0]))" \
+  init                                  \
   --insecure
