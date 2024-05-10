b24a1b2ac897b567e7a5fecd6bcdef889af4b4e1
Przemysław Szymański <przemyslaw.szymanski@flytronic.pl>
2024-05-10 08:39:18 +0200

    build(app): fixed tournament script

--- projects/tournament.sh
+++ projects/tournament.sh
@@ -12,33 +12,34 @@ docker network create \
   --gateway=20.0.0.1 \
   "${network}" &>/dev/null
 
+docker container prune --force &>/dev/null
+docker image prune --force &>/dev/null
+
 # Run a containerized NGINX
 
 image="nginx:latest"
+name="nginx"
 ports=(80 443)
 
 source ../utils/port-check.sh "${ports[0]}" "${ports[1]}"
 
 docker pull "$image" &>/dev/null
 
-docker container prune --force &>/dev/null
-docker image prune --force &>/dev/null
+docker stop "${name}" &>/dev/null
+docker rm "${name}" &>/dev/null
 
-docker stop nginx &>/dev/null
-docker rm nginx &>/dev/null
-
-docker run                                                            \
-  --name nginx                                                        \
-  --restart always                                                    \
-  --detach                                                            \
-  --restart always                                                    \
-  --network "${network}"                                              \
-  --ip 20.0.0.2                                                       \
-  --volume /etc/ssl:/etc/ssl                                          \
-  --volume ./nginx/conf.d/default.conf:/etc/nginx/conf.d/default.conf \
-  --publish 0.0.0.0:"${ports[0]}":"${ports[0]}"                       \
-  --publish 0.0.0.0:"${ports[1]}":"${ports[1]}"                       \
-  "$image" &>/dev/null
+docker run \
+  --name "${name}" \
+  --restart always \
+  --detach \
+  --restart always \
+  --network "${network}" \
+  --ip 20.0.0.2 \
+  --volume /etc/ssl:/etc/ssl \
+  --volume ./../"${name}"/conf.d:/etc/"${name}"/conf.d \
+  --publish 0.0.0.0:"${ports[0]}":"${ports[0]}" \
+  --publish 0.0.0.0:"${ports[1]}":"${ports[1]}" \
+  "${image}" &>/dev/null
 
 # Run a containerized Node
 
@@ -47,17 +48,14 @@ name="tournament_app"
 
 docker pull "${image}" &>/dev/null
 
-docker container prune --force &>/dev/null
-docker image prune --force &>/dev/null
-
 docker stop "${name}" || true
 docker rm "${name}" || true
 
-docker run               \
-  --name "${name}"       \
-  --detach               \
-  --restart always       \
+docker run \
+  --name "${name}" \
+  --detach \
+  --restart always  \
   --network "${network}" \
-  --ip 20.0.0.3          \
+  --ip 20.0.0.3 \
   "${image}" &>/dev/null
 
