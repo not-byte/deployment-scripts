95815de2348c80d91a7881cfbd9baea8312f6d06
botprzemek <info@botprzemek.pl>
2024-07-12 09:29:29 +0200

    feat(app): changed crontab

--- utils/repo-check.sh
+++ utils/repo-check.sh
@@ -3,13 +3,22 @@
 # Check GitHub repository for updates
 
 repository="deployment-scripts"
-
-git status --uno
+hasChanged="false"
 
 cd ~/"${repository}" || exit 1
 
-git pull &>/dev/null
+git fetch
 
-crontab crontab || exit 1
+git status -uno | grep -q "behind" && hasChanged="true"
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')]: Repository ${repository} updated successfully."
+if [ "${hasChanged}" ];
+then
+  git pull &>/dev/null
+
+  crontab crontab || exit 1
+
+  message="[$(date '+%Y-%m-%d %H:%M:%S')]: Repository ${repository} updated successfully."
+
+  write "${message}"
+  echo "${message}"
+fi
